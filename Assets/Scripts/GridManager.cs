4821697410d838cbffe53ab13acce3b6c51a56c4
Treasure Uzondu <uzondut@gmail.com>
2025-12-12 23:41:16 -0500

    added Player movement

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,25 +1,44 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour {
-    [SerializeField] private int _width, _height;
+    [SerializeField] private Player _playerPrefab;
     [SerializeField] private Tile _tilePrefab; 
     [SerializeField] private Transform _cam;
+    [SerializeField] private int _width, _height;
+    public int GetWidth => this._width;
+    public int GetHeight => this._height;
     
+    public static GridManager instance;
+    
+    private Dictionary<Vector2, Tile> _tiles;
     private Tile _spawnedTile;
 
+    void Awake() {
+        instance = this;
+    }
+    
     void Start() {
+        this._tiles = new Dictionary<Vector2, Tile>();
+        Instantiate(this._playerPrefab, this._playerPrefab.transform.position, Quaternion.identity);
         GenerateGrid();
     }
     
     void GenerateGrid() {
-        for (int i = 0; i < this._width; i++) {
-            for (int j = 0; j < this._height; j++) {
-                this._spawnedTile = Instantiate(this._tilePrefab, new Vector2(i, j), Quaternion.identity);
-                
-                bool isOffset = (i % 2 == 0 && j  % 2 != 0) || (i % 2 != 0 && j  % 2 == 0);
-                this._spawnedTile.Init(isOffset);
+        for (int x = 0; x < this._width; x++) {
+            for (int y = 0; y < this._height; y++) {
+                this._spawnedTile = Instantiate(this._tilePrefab, new Vector3(x, y), Quaternion.identity);
+                this._spawnedTile.Init(x, y);
+                this._tiles.Add(new Vector2(x, y), this._spawnedTile);
             }
         }
-        this._cam.position = new Vector3((float) this._width/2 - 0.5f, (float) this._height/2, -10);
+        // Centering the cam position on the grid (based on the width and height)
+        this._cam.position = new Vector3((float) this._width/2 - 0.5f, (float) this._height/2 - 0.5f, -10); 
     }
+    
+    public Tile GetTileAtPosition(Vector2 position) {
+        return this._tiles.GetValueOrDefault(position);
+    }
+
+    
 }
